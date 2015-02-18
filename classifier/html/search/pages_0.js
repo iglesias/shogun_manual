4d5a0660f1ad5a7a45315fc56cb3b1c9c98f42e7
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-02-18 19:55:34 +0000

    removed accidentially added files

--- classifier/html/search/pages_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['k_2dnearest_20neighbours',['K-Nearest Neighbours',['../md_knn.html',1,'']]],
-  ['k_2dnearest_20neighbours',['K-Nearest Neighbours',['../md_knn_octave.html',1,'']]],
-  ['k_2dnearest_20neighbours',['K-Nearest Neighbours',['../md_knn_python.html',1,'']]]
-];

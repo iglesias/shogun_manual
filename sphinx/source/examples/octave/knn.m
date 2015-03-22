319d55b0991f5d402a6e90d6b98edd50d2547007
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-22 21:31:51 +0000

    update files

---
+++ sphinx/source/examples/octave/knn.m
@@ -0,0 +1,26 @@
+modshogun
+
+%![load_data]
+trainf = CSVFile('../data/fm_train_real.dat');
+feats_train = RealFeatures(trainf);
+testf = CSVFile('../data/fm_test_real.dat');
+feats_test = RealFeatures(testf);
+train_labels = CSVFile('../data/label_train_multiclass.dat');
+labels = MulticlassLabels(train_labels);
+%![load_data]
+
+%![choose_distance]
+distance = EuclideanDistance(feats_train, feats_test);
+%![choose_distance]
+
+%![create_instance]
+knn = KNN(3, distance, labels);
+%![create_instance]
+
+%![train_and_apply]
+knn.train();
+test_labels = knn.apply(feats_test);
+output = test_labels.get_values();
+disp(output);
+%![train_and_apply]
+
---
+++ sphinx/source/index.rst
@@ -0,0 +1,22 @@
+.. Shogun-manual documentation master file, created by
+   sphinx-quickstart on Fri Mar 20 22:34:40 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Shogun-manual's documentation!
+=========================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   knn
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+

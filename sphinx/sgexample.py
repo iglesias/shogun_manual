fc7dc7d90ecd0f32901ac730d913e6886a4f91ae
Sergey Lisitsyn <lisitsyn.s.o@gmail.com>
2015-03-23 11:21:38 +0300

    Added nodes for sgexmaple

--- sphinx/sgexample.py
+++ sphinx/sgexample.py
@@ -2,9 +2,9 @@ def setup(app):
     app.add_config_value('sgexample_include_examples', False, True)
 
     app.add_node(sgexample,
-				html=(visit_sgexample_node, depart_sgexample_node),
-				latex=(visit_sgexample_node, depart_sgexample_node),
-				text=(visit_sgexample_node, depart_sgexample_node))
+            html=(visit_sgexample_node, depart_sgexample_node),
+            latex=(visit_sgexample_node, depart_sgexample_node),
+            text=(visit_sgexample_node, depart_sgexample_node))
 
     app.add_directive('sgexample', ShogunExample)
 
@@ -20,66 +20,64 @@ from docutils import nodes
 class sgexample(nodes.Admonition, nodes.Element):
     pass
 
+# todo how to implement a custom writer for that?
+class SelectableContainer(nodes.container):
+    pass
+
+# todo how to implement a custom writer for that?
+class ShogunCode(nodes.Node):
+    pass
+
 from sphinx.directives.code import LiteralInclude
 import os
 class ShogunExample(LiteralInclude):
-	def run(self):
-		result = []
-		# save original node
-		orig_fname = self.arguments[0].strip()
-		orig_language = self.options['language'].strip()
-		
-		# create nodes with parsed listings
-		for target in get_supported_languages():
-			self.arguments[0] = filename_sg_to_target(orig_fname, target)
-			self.options['language'] = target
-			
-			# call base class, returns list
-			result += LiteralInclude.run(self)
-		
-		# restore
-		self.arguments[0] = orig_fname
-		self.options['language'] = orig_language
-		
-		return result
+    def run(self):
+	result = SelectableContainer()
+	# save original node
+	orig_fname = self.arguments[0].strip()
+	orig_language = self.options['language'].strip()
 
-def filename_sg_to_target(fname, target):
-	# extract filename from unicode
-	splitted = str(fname).split(os.sep)
-	directory = os.sep.join(splitted[:-1])
-	fname_base = splitted[-1][:-3]
+	# create nodes with parsed listings
+	for target, extension in get_supported_languages():
+	    self.arguments[0] = filename_sg_to_target(orig_fname, target, extension)
+	    self.options['language'] = target
+	    # call base class, returns list
+	    sgcode = ShogunCode()
+	    sgcode += LiteralInclude.run(self)
+	    result += sgcode
 
-	# make sure the format is like "knn.sg"
-	if not splitted[-3:] != ".sg":
-		raise ValueError("Could not parse example filename \"%s\"" % fname)
+	# restore
+	self.arguments[0] = orig_fname
+	self.options['language'] = orig_language
 
-	# construct actual code filename
-	suffix = get_language_file_suffix(target)
-	return directory + os.sep + target + os.sep + fname_base + "." + suffix
+	return [result]
 
-def get_language_file_suffix(target):
-	if target == "python":
-		return "py"
-	elif target == "octave":
-		return "m"
-	else:
-		raise ValueError("Unsupported Shogun target language: %s" % target)
+def filename_sg_to_target(fname, target, extension):
+    # extract filename from unicode
+    splitted = str(fname).split(os.sep)
+    directory = os.sep.join(splitted[:-1])
+    fname_base = splitted[-1][:-3]
+
+    # make sure the format is like "knn.sg"
+    if not splitted[-3:] != ".sg":
+	raise ValueError("Could not parse example filename \"%s\"" % fname)
+
+    # construct actual code filename
+    return os.path.join(directory, target, "%s.%s" % (fname_base, extension))
 
 def get_supported_languages():
-	return [
-			"python",
-			"octave",
-			]
+    return (("python", "py"),
+            ("octave", "m"))
 
-from docutils.statemachine import ViewList
+    from docutils.statemachine import ViewList
 def container_wrapper(directive, literal_node, caption):
     container_node = nodes.container('', literal_block=True,
-                                     classes=['literal-block-wrapper'])
+            classes=['literal-block-wrapper'])
     parsed = nodes.Element()
     directive.state.nested_parse(ViewList([caption], source=''),
-                                 directive.content_offset, parsed)
+            directive.content_offset, parsed)
     caption_node = nodes.caption(parsed[0].rawsource, '',
-                                 *parsed[0].children)
+            *parsed[0].children)
     caption_node.source = parsed[0].source
     caption_node.line = parsed[0].line
     container_node += caption_node
